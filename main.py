ee08d63f8e4f42ce3fb31087fea327370c7ce5e6
Jana <yanix2x2@gmail.com>
2025-05-29 16:28:44 +0300

    changed variable name

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ django.setup()
 from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
-    passcard_amount = Passcard.objects.count()
-    active_passcard = Passcard.objects.filter(is_active=True).count()
-    print('Всего пропусков: ', passcard_amount)
-    print('Активных пропусков: ', active_passcard)
+    total_passcard_amount = Passcard.objects.count()
+    active_passcards = Passcard.objects.filter(is_active=True).count()
+    print('Всего пропусков: ', total_passcard_amount)
+    print('Активных пропусков: ', active_passcards)
